9d5a87482ec40ad4180a0330a501e27aa846b60d
manemajef <rotem.semah@gmail.com>
2025-11-05 20:51:59 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# move pdf's from downloads
---
+++ main.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import shutil
+
+download_dir = Path.home() / "Downloads"
+tau_dir = Path.home() / "TAU"
+
+
+def put_file(name):
+    path = tau_dir
+    name = name.split(".")[0]
+    name_parts = name.split("-")
+
+    class_found = False
+
+    for part in name_parts:
+        if len(path.parts) - len(tau_dir.parts) >= 3:
+            return path
+
+        if not class_found and part in ["micro", "macro", "linear", "discrete", "econ"]:
+            path = path / part
+            class_found = True
+
+        if class_found and part in ["lec", "rec", "hw"]:
+            path = path / (part + "s")
+            break
+        if not class_found:
+            return download_dir
+
+    return path
+
+
+def main():
+    for file in download_dir.iterdir():
+        name = file.name
+        path = put_file(name)
+        if path != download_dir:
+            if not path.exists():
+                raise FileNotFoundError("")
+            print(f"moving {file.name} to {path}")
+            shutil.move(str(file), str(path / file.name))
+
+
+if __name__ == "__main__":
+    main()
